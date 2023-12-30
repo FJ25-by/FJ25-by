4cbf80ece6dece6e8d436379f420eee9b1f27ff2
FJ25-by <142897924+FJ25-by@users.noreply.github.com>
2023-12-30 20:39:57 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import moment from 'moment-timezone'
+let wibh = moment.tz('Asia/Jakarta').format('HH')
+    let wibm = moment.tz('Asia/Jakarta').format('mm')
+    let wibs = moment.tz('Asia/Jakarta').format('ss')
+    let wktuwib = `${wibh}:${wibm}:${wibs}`
+
+// Hari Tanggal
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+console.log('HAI SEMUA APA KABAR')
+console.log(date)
